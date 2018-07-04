5f2ec230ee4b6e5f9a6f1a625889477740dd0af4
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-04 04:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var name = "Susan"
 var height = 74
 var message = ` is ${height} inches tall`
 
-if (name === "Susan") {
-    console.log("The name is correct")
+if (height === 74) {
+    
 } else {
     console.log("Expected the name to be Susan")
 }
