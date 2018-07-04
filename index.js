84ef5d3b20b32c788176d4e8fbd5b0eb1013cbd8
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-04 04:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var height = 74
 var message = ` is ${height} inches tall`
 
 if (name === "Susan") {
-   console.log(height 74 ) 
+   console.log(height = 74) 
+
+  
 } else {
     console.log("Wrong")
 }
