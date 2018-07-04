70cab56003f567f25e1fab587e6aeac0d49e22d3
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-04 04:40:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // write your code below!
 var name = "Susan"
 var height = 74
-var message = ` is ${height} inches tall`
+var message = `${name} is ${height} inches tall`
 
-if (name === "Susan") {
-   console.log(height = 74) 
 
-}
 
 
 module.exports = {name, height, message}
